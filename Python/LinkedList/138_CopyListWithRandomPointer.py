a63a162ddcd34b4eb5adf1eb6a1e83c22be8968e
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-06 08:29:52 -0800

    Update 138_CopyListWithRandomPointer.py

--- Python/LinkedList/138_CopyListWithRandomPointer.py
+++ Python/LinkedList/138_CopyListWithRandomPointer.py
@@ -1,17 +1,41 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, next, random):
-        self.val = val
-        self.next = next
-        self.random = random
-"""
-#Solution 1: O(n), O(1)
-# reference: https://zhuanlan.zhihu.com/p/38888164
+# """
+# # Definition for a Node.
+# class Node:
+#     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+#         self.val = int(x)
+#         self.next = next
+#         self.random = random
+# """
+
+# # Method 1 - Hashmap, dict, space complexity o(n)
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        m, n = head, head
+        dic = dict()
+        # dic[None] = None
+        while m:
+            dic[m] = Node(m.val)
+            m = m.next
+        while n:
+            new_node = dic[n]
+            # if n.next == None or n.random == None
+            # dic.get(None) return return None
+            # or we can use init dic[None] = None to solve this
+            new_node.next = dic.get(n.next)
+            new_node.random = dic.get(n.random)
+            # new_node.next = dic[n.next]
+            # new_node.random = dic[n.random]
+            n = n.next
+       # return dic[head]
+        return dic.get(head)
+        
+
+# Method 2 - Copy directly
 class Solution:
     def copyRandomList(self, head):
         if head is None:
             return None
+        
         # copy every node, link them to a linked list
         curr = head
         while curr:
@@ -19,6 +43,7 @@ class Solution:
             node.next = curr.next
             curr.next = node
             curr = curr.next.next
+            
         # copy the random pointer
         curr = head
         while curr:
@@ -28,29 +53,14 @@ class Solution:
             else:
                 node.random = curr.random.next
             curr = curr.next.next
+            
         # split the copy link
         curr = head
         newHead = head.next
         while curr:
             node = curr.next
             curr.next = node.next
-            if node.next is None:
-                node.next = None
-            else:
+            if node.next:
                 node.next = node.next.next
             curr = curr.next
         return newHead
-
-#Solution 2: O(n), O(n)
-# class Solution:
-#     def copyRandomList(self, head):
-#         dic = dict()
-#         m = n = head
-#         while m:
-#             dic[m] = Node(m.val, None, None)
-#             m = m.next
-#         while n:
-#             dic[n].next = dic.get(n.next)
-#             dic[n].random = dic.get(n.random)
-#             n = n.next
-#         return dic.get(head)
