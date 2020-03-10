74f1613b54940bb8bad3c5bbf4d7b6bcbc6a0b0b
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-10 08:40:25 -0700

    Add files via upload

---
+++ Python/LinkedList/2_Add_Two_Numbers.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        head = ListNode(0)
+        curr = head
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, digit = divmod(v1 + v2 + carry, 10)
+
+            curr.next = ListNode(digit)
+            curr = curr.next
+        return head.next
