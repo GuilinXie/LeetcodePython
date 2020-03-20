3f5bb98d0631aae504cc869e905a904e15de9fb9
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-20 10:19:50 -0700

    Update 1233_RemoveSubFoldersFromTheFilesystem.py

--- Python/Trie/1233_RemoveSubFoldersFromTheFilesystem.py
+++ Python/Trie/1233_RemoveSubFoldersFromTheFilesystem.py
@@ -51,6 +51,41 @@ class Solution(object):
 #                 ans.append(f)
 #         return ans
 
+# Solution 3 - Standard buildTrie method
+
+class Solution:
+    
+    def __init__(self):
+        self.root = dict()
+    
+    def removeSubfolders(self, A: List[str]) -> List[str]:
+        if len(A) <= 1:
+            return A   
+        rows = []
+        for row in A:
+            row = row.strip("/").split("/")
+            rows.append(row)
+        rows = sorted(rows, key=len)
+        
+        res = []
+        self.buildTrie(rows, res)
+        return ["/" + "/".join(row) for row in res]
+    
+    def buildTrie(self, folders, res):        
+     
+        for folder in folders:
+            p = self.root
+            for c in folder:
+                if "#" in p:
+                    break
+                if c not in p:
+                    p[c] = dict()         
+                p = p[c]
+            if "#" not in p: 
+                p["#"] = "#"
+                res.append(folder)
+            
+
 if __name__ == "__main__":
     s = Solution()
     res = s.removeSubfolders(["/a","/a/b","/c/d","/c/d/e","/c/f"])
