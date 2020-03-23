a9504f9b57fb67df7edb6abbe0054403fffb4550
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-22 21:30:00 -0700

    Add files via upload

---
+++ Python/Arrary/204_Count_Primes.py
@@ -0,0 +1,28 @@
+import math
+
+
+class Solution:
+
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 0:
+            return 0
+
+        # do not need to reset isPrime[0] = False or isPrime[1] = False
+        # because we can skip these 2 cases in range(2, n)
+        isPrime = [True] * n
+
+        for i in range(2, n):
+            if isPrime[i] == False:
+                continue
+            for j in range(i * i, n, i):
+                isPrime[j] = False
+
+        ans = 0
+        for i in range(2, n):
+            if isPrime[i]:
+                ans += 1
+        return ans
