e553c0d10301defc357db5cf1d113cfddc8b0d2e
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-29 12:02:08 -0700

    Update 1311_Get_Watched_Videos_by_Your_Friends.py

--- Python/BFS/1311_Get_Watched_Videos_by_Your_Friends.py
+++ Python/BFS/1311_Get_Watched_Videos_by_Your_Friends.py
@@ -1,3 +1,39 @@
+# 2nd version - beat 80%
+# more readability
+from collections import Counter
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos, friends, idx, level):
+        if (len(watchedVideos) <= 0 or len(friends) <= 0 or idx < 0 or level < 0):
+            return
+        graph = {}
+        for i in range(0, len(friends)):
+            graph[i] = friends[i]
+            
+        dq = collections.deque([(idx, 0)])
+        seen = set([idx])
+        cand = []
+        while dq:
+            cur, dist = dq.popleft()
+            if dist == level:
+
+                cand.append(cur)
+            neighbors = graph[cur]
+            for nei in neighbors:
+                if nei not in seen:
+                    seen.add(nei)
+                    dq.append((nei, dist + 1))
+          
+        watchedTotal = []
+        for f in cand:
+            watchedTotal += watchedVideos[f]
+            
+        count = Counter(watchedTotal)
+        sortCount = sorted(count.items(), key=lambda item:(item[1],item[0]))
+        ans = [k for k, v in sortCount]
+        return ans
+
+# original
+# do not need level_num, instead using deque && popleft() to control the order
 from collections import Counter
 class Solution:
     def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
