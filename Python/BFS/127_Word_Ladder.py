933bb22154fc72e6d2007aac5a72d6b6f4af025d
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-29 11:23:19 -0700

    Update 127_Word_Ladder.py

--- Python/BFS/127_Word_Ladder.py
+++ Python/BFS/127_Word_Ladder.py
@@ -1,3 +1,48 @@
+# 2nd version - update seen for every neighbor before appending to queue, can be faster
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        wordList.append(beginWord)
+        
+        def buildDict(wordSet):
+            wordDict = {}
+            for word in wordSet:
+                for i in range(0, len(word)):
+                    key = word[:i] + "_" + word[i+1:]
+                    if key not in wordDict:
+                        wordDict[key] = []
+                    wordDict[key].append(word)
+            return wordDict
+        
+        wordDict = buildDict(wordList)
+        
+        dq = collections.deque([(beginWord, 1)])
+        seen = set([beginWord])
+        while dq:
+            word, dist = dq.popleft()
+           # not fast if update seen here
+           # seen.add(word)
+            if word == endWord:
+                return dist
+            for i in range(0, len(word)):
+                key = word[:i] +  "_" + word[i+1:]
+                if key not in wordDict:
+                    continue
+                else:
+                    for nei in wordDict[key]:
+                        if nei not in seen:
+                            dq.append((nei, dist + 1))
+                            # update seen here for every neighbor, can be faster
+                            seen.add(nei)
+        return 0
+       
+
+# original
 class Solution(object):
     def ladderLength(self, beginWord, endWord, wordList):
 
