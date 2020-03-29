0894c915f088e7233448252ac245dcb603a087ce
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-29 10:56:58 -0700

    Update 994_Rotting_Oranges.py

--- Python/BFS/994_Rotting_Oranges.py
+++ Python/BFS/994_Rotting_Oranges.py
@@ -1,3 +1,35 @@
+# 2nd version - AC - beat 90%
+class Solution:
+    def orangesRotting(self, A):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if len(A) <= 0 or len(A[0]) <= 0:
+            return 0
+        R, C = len(A), len(A[0])
+        
+        initial = [(i, j, 0) for i in range(R) for j in range(C) if A[i][j] == 2]
+        dq = collections.deque(initial)
+        ans = 0
+        while dq:
+            i, j, time = dq.popleft()
+            ans = time
+            neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+            for new_i, new_j in neighbors:
+                if 0 <= new_i < R and 0 <= new_j < C and A[new_i][new_j] == 1:
+                    dq.append((new_i, new_j, time + 1))
+                    A[new_i][new_j] = 2
+                    
+        for i in range(R):
+            for j in range(C):
+                if A[i][j] == 1:
+                    return -1
+        return ans
+
+
+
+# original
 class Solution:
     def orangesRotting(self, A):
         if len(A) <= 0 or len(A[0]) <= 0:
