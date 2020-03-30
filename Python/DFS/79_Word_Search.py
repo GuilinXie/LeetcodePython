b16cc4de3bcac0ad12423619bdea8364196333c6
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-29 20:59:57 -0700

    Update 79_Word_Search.py

--- Python/DFS/79_Word_Search.py
+++ Python/DFS/79_Word_Search.py
@@ -1,3 +1,39 @@
+# 2nd version, O(N * 4^L), where N = m * n of the grid, 4^L where L is the length of the word.
+class Solution:
+    # Method: dfs + backtracd, can fail for adding visit set, so just change grid directly
+    # For backtracking, do not check nei before going into dfs
+    # evey nei going into the dfs, then check the borders at the begining of the dfs
+    def exist(self, grid: List[List[str]], word: str) -> bool:
+        m = len(grid)
+        n = len(grid[0])
+        if m <= 0 or n <= 0:
+            return False
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] != word[0]:
+                    continue
+                res = self.search(grid, i, j, word)
+                if res:
+                    return res
+        return False
+
+    def search(self, grid, i, j, s):
+        if not s:
+            return True
+
+        if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == s[0]:
+            tmp = grid[i][j]
+            grid[i][j] = "#"
+            neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+            for new_i, new_j in neighbors:
+                r = self.search(grid, new_i, new_j, s[1:])
+                if r:
+                    return True
+            grid[i][j] = tmp
+        return False
+
+
+# original - actually, do not need to add cache to pass, just change gird directly for backtrack, can save time
 # Time complexity O(N * 4^L), where N = m * n of the grid, 4^L where L is the length of the word.
 class Solution:
     # Method: DFS + cache, can fail for time complexity if no cache
