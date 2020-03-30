2d00c99a0e07d9768fa2ce11f2616b2b8fee1d21
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-29 21:46:41 -0700

    Update 297_Serialize_and_Deserialize_Binary_Tree.py

--- Python/Tree/297_Serialize_and_Deserialize_Binary_Tree.py
+++ Python/Tree/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -5,21 +5,6 @@
 #         self.left = None
 #         self.right = None
 
-# Method 1 - Using DFS - preOrder to serialize and deserialize
-# But the problem is that it returns tuple for the serialize function, should return str
-class Codec:
-    def serialize(self, root):
-        if not root:
-            return 'x'
-        return root.val, self.serialize(root.left), self.serialize(root.right)
-
-    def deserialize(self, data):
-        if data[0] == 'x':
-            return None
-        node = TreeNode(data[0])
-        node.left = self.deserialize(data[1])
-        node.right = self.deserialize(data[2])
-        return node
 
 
 # Method 1 - Update, change serialize return to str
@@ -48,6 +33,23 @@ class Codec:
         root.left = self.deserializeDfs(data)
         root.right = self.deserializeDfs(data)
         return root
+
+    
+# Method 1 - Using DFS - but not return str - preOrder to serialize and deserialize
+# But the problem is that it returns tuple for the serialize function, should return str
+class Codec:
+    def serialize(self, root):
+        if not root:
+            return 'x'
+        return root.val, self.serialize(root.left), self.serialize(root.right)
+
+    def deserialize(self, data):
+        if data[0] == 'x':
+            return None
+        node = TreeNode(data[0])
+        node.left = self.deserialize(data[1])
+        node.right = self.deserialize(data[2])
+        return node
 
 
 # Method 2 - Using BFS - level order to serialize and deserialize
