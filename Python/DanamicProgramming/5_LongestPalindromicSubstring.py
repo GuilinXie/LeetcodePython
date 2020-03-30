8a1df7758a7ea67c79c53bae325882220a73475a
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-29 21:24:49 -0700

    Update 5_LongestPalindromicSubstring.py

--- Python/DanamicProgramming/5_LongestPalindromicSubstring.py
+++ Python/DanamicProgramming/5_LongestPalindromicSubstring.py
@@ -1,5 +1,35 @@
 # https://leetcode.com/problems/longest-palindromic-substring/discuss/151144/Bottom-up-DP-Logical-Thinking
 
+
+# This is for the answer Java
+class Solution {
+public String longestPalindrome(String s) {
+    if (s == null || s.length() < 1) 
+        return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int len1 = expandAroundCenter(s, i, i);
+        int len2 = expandAroundCenter(s, i, i + 1);
+        int len = Math.max(len1, len2);
+        if (len > end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return s.substring(start, end + 1);
+}
+
+private int expandAroundCenter(String s, int left, int right) {
+    int L = left, R = right;
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+        L--;
+        R++;
+    }
+    return R - L - 1;
+}
+}
+
+
 class Solution:
     # def longestPalindrome(self, s):
     #     if len(s) <= 1:
@@ -23,6 +53,7 @@ class Solution:
     def __init__(self):
         self.dp = None
 
+    # this is for another similar question findLongestCommon between s1 and s2
     def findLongestCommon(self, s1, s2):
         if len(s1) <= 0 or len(s2) <= 0:
             return
