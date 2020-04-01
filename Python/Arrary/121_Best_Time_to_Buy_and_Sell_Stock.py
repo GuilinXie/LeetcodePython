463a38604bfa99dbc980609e0ae391a858ef68bb
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-03-31 22:11:47 -0700

    Update 121_Best_Time_to_Buy_and_Sell_Stock.py

--- Python/Arrary/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ Python/Arrary/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,6 +1,22 @@
-class Solution:
 
-    # Method 1 : BF, O(n*2), O(1)
+# Solution 1 - best one so far - AC - beat 80%
+class Solution:
+    # Method 3: best one, O(n), O(1)
+    def maxProfit(self, prices):
+        if len(prices) <= 0:
+            return 0
+        min_price = sys.maxsize
+        max_profit = 0
+        for p in prices:
+            if p < min_price:
+                min_price = p
+            elif p - min_price > max_profit:
+                max_profit = p - min_price
+        return max_profit
+
+
+class Solution:
+    # Method 2 : BF, O(n*2), O(1)
     def maxProfit(self, prices: List[int]) -> int:
         if len(prices) <= 0:
             return 0
@@ -12,7 +28,7 @@ class Solution:
                 res = profit
         return res
 
-    # Method 2: dp, similar to trapping rain water right side, O(n), O(n)
+    # Method 3: dp, similar to trapping rain water right side, O(n), O(n)
     def maxProfit(self, prices):
         if len(prices) <= 0:
             return 0
@@ -28,16 +44,3 @@ class Solution:
             if profit > res:
                 res = profit
         return res
-
-    # Method 3: best one, O(n), O(1)
-    def maxProfit(self, prices):
-        if len(prices) <= 0:
-            return 0
-        min_price = sys.maxsize
-        max_profit = 0
-        for i in range(0, len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
-        return max_profit
