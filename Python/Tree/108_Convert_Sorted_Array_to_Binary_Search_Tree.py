9e9f56043843ccba293b37bef5657f4cbaa625e5
GuilinXie <43485626+GuilinXie@users.noreply.github.com>
2020-04-20 12:16:03 -0700

    Update 108_Convert_Sorted_Array_to_Binary_Search_Tree.py

--- Python/Tree/108_Convert_Sorted_Array_to_Binary_Search_Tree.py
+++ Python/Tree/108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -5,6 +5,26 @@
 #         self.left = None
 #         self.right = None
 
+
+# Solution 1 - beat 75%, easier to find out with a helper buildBST function
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if len(nums) <= 0:
+            return None
+        root = self.buildBST(nums, 0, len(nums) - 1)
+        return root
+    
+    def buildBST(self, nums, low, high):
+        if low > high:
+            return None
+        mid = (low + high + 1) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.buildBST(nums, low, mid - 1)
+        root.right = self.buildBST(nums, mid + 1, high)
+        return root
+    
+    
+# Solution 2 - hard to find out later
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
         if len(nums) <= 0:
