4092132cd77277482f490580bf4c9ef5fc89c442
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-07-06 07:24:37 -0700

    Update 39_Combination_Sum.py

--- Python/DFS/39_Combination_Sum.py
+++ Python/DFS/39_Combination_Sum.py
@@ -1,3 +1,11 @@
+# Time: O(l^k)
+# Where l is the length of the array, k is the length of the longest possible combination (namely target / minInArray).
+# i.e. If the min value in array is 1, and target is 9, the longest length of possible combination is 9 (9/1)
+
+# Space: Space: O(k)
+# For storing the path, which could be k long at most.
+
+# beat 40%
 class Solution:
     def combinationSum(self, candidates, target):
         res = []
