d134ec5abf958218bdddec060a6cdcf1f91ff4bf
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-07-17 06:48:06 -0700

    Add files via upload

--- Python/String/5_LongestPalindromicSubstring.py
+++ Python/String/5_LongestPalindromicSubstring.py
@@ -1,6 +1,42 @@
 # https://leetcode.com/problems/longest-palindromic-substring/discuss/151144/Bottom-up-DP-Logical-Thinking
 
 class Solution:
+
+# solution 1 - Brute Force
+    # def longestPalindrome(self, s: str) -> str:
+    #     if len(s) <= 0:
+    #         return ""
+    #     ans = ""
+    #     for i in range(len(s)):
+    #         for j in range(i, len(s)+1):
+    #             subStr = s[i:j+1]
+    #             reverse = subStr[::-1]
+    #             if subStr == reverse and len(subStr) > len(ans):
+    #                 ans = subStr
+    #     return ans
+    
+# solution 2 - expanding
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+        start, end = 0, 0
+        for i in range(len(s)):
+            len1 = self.expandAroundCenter(s, i, i)
+            len2 = self.expandAroundCenter(s, i, i + 1)
+            length = max(len1, len2)
+            if length > end - start:
+                start = i - (length - 1) // 2
+                end = i + length // 2
+        return s[start:end + 1]
+
+    def expandAroundCenter(self, s, i, j):
+        while i >= 0 and j < len(s) and s[i] == s[j]:
+            i -= 1
+            j += 1
+        return j - i - 1
+
+# solution 3 - dp
+class Solution:
     def longestPalindrome(self, s):
         if len(s) <= 1:
             return s
