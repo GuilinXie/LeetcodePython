ac2089791f7a1f64cfcfb5b4b520dba978d571bb
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-08-07 11:50:51 -0700

    Create 43_Multiply_Strings.py

---
+++ Python/Arrary/43_Multiply_Strings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        m = len(num1)
+        n = len(num2)
+        pos = [0] * (m + n)
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = int(num1[i]) * int(num2[j])
+                p1 = i + j
+                p2 = i + j + 1
+                
+                s = mul + pos[p2]
+                
+                pos[p1] += s // 10
+                pos[p2] = s % 10
+            
+        ans = ''.join([str(i) for i in pos])
+        if ans.lstrip("0") != "":
+            ans = ans.lstrip("0")
+        else:
+            ans = "0"
+        return ans
