9f5ef882a3bc9e16c9623b1764e71a07865d18ba
LynnXie <43485626+GuilinXie@users.noreply.github.com>
2020-08-10 09:29:52 -0700

    Create 47_Permutations_2.py

---
+++ Python/DFS/47_Permutations_2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permuteUnique(self, nums):
+        res = []
+        nums.sort()
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self, nums, tmp, res):
+        if not nums:
+            res.append(tmp)
+        for i in range(len(nums)):
+            if i != 0 and nums[i] == nums[i - 1]:
+                continue
+            self.dfs(nums[:i] + nums[i+1:], tmp + [nums[i]], res)
